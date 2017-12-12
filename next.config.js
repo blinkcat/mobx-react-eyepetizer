388e4fc4250e0d2671051867d6dd857d5beb86ae
blinkcat <1209311663@qq.com>
2017-12-12 15:55:12 +0800

    add gh-pages assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   exportPathMap: function() {
     return {
       '/': { page: '/' },
       '/detail': { page: '/detail' }
     };
-  }
+  },
+  // for gh-pages
+  assetPrefix: isProd ? '/mobx-react-eyepetizer/' : ''
 };
