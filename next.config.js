e5a926a6b389690767b415987fddebb9b2303ca3
blinkcat <1209311663@qq.com>
2017-12-14 23:15:15 +0800

    add webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const prod = process.env.NODE_ENV === 'production';
 const DEPLOY_PREFIX = prod ? '/mobx-react-eyepetizer' : '';
+const { ANALYZE } = process.env;
 
 module.exports = {
   exportPathMap: function() {
@@ -11,6 +13,15 @@ module.exports = {
   // for gh-pages
   assetPrefix: DEPLOY_PREFIX,
   webpack: function(config) {
+    if (ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: 8888,
+          openAnalyzer: true
+        })
+      );
+    }
     // feel bad  https://github.com/zeit/next.js/issues/2759
     config.output.publicPath = `${DEPLOY_PREFIX}${config.output.publicPath}`;
     return config;
